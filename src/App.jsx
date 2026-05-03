a690b388dccbf8d3dc101c467f30c5c409458ea6
Sofya Asinskaya <sofya.asinskaya@stud.esdc.lt>
2026-05-03 08:55:35 +0300

    Revert "login"
    This reverts commit 8ae0f41d31e5e070d4cc956875d076eba425be0f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
-import { useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import Header from "./components/Header/Header";
-import HomePage from "./pages/Home/HomePage";
-import EventMapPage from "./pages/MapView/MapPage";
-import LoginPage from "./pages/Login/LoginPage";
-import EventDetailPage from "./pages/EventDetail/EventDetail";
-import RegisterPage from "./pages/Register/RegisterPage";
-import ProfilePage from "./pages/Profile/ProfilePage";
-import EventPage from "./pages/MyEvents/EventPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   return (
-    <>
-      <Header 
-        isLoggedIn={isLoggedIn} 
-        setIsLoggedIn={setIsLoggedIn} 
-      />
-
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-
-        <Route
-          path="/login"
-          element={<LoginPage setIsLoggedIn={setIsLoggedIn} />}
-        />
-
-        <Route path="/register" element={<RegisterPage />} />
-
-        <Route
-          path="/map"
-          element={isLoggedIn ? <EventMapPage /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/events/:id"
-          element={isLoggedIn ? <EventDetailPage /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/my-events"
-          element={isLoggedIn ? <EventPage /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/profile"
-          element={isLoggedIn ? <ProfilePage /> : <Navigate to="/login" />}
-        />
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/map" element={<div>Map</div>} />
+        <Route path="/events/:id" element={<div>Event Detail</div>} />
+        <Route path="/my-events" element={<div>My Events</div>} />
+        <Route path="/profile" element={<div>Profile</div>} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
