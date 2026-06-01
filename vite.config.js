c8112f1761b78b5eab2abea383a566cc5978b1d3
Sofya Asinskaya <sofya.asinskaya@stud.esdc.lt>
2026-06-01 12:05:56 +0300

    chore: #145-#150-apply-port-and-domain-config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [
+      "ec2-51-20-182-189.eu-north-1.compute.amazonaws.com",
+    ],
     proxy: {
       "/api": {
         target: "http://51.21.218.154:8080",
@@ -20,5 +23,12 @@ export default defineConfig({
     allowedHosts: [
       "ec2-51-20-182-189.eu-north-1.compute.amazonaws.com",
     ],
+    proxy: {
+      "/api": {
+        target: "http://51.21.218.154:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
