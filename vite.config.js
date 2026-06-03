70a093738ef2225a428258dc7ae5ae9f333daf6f
Uladzislau Mikhayevich <uladzislau.mikhayevich@stud.esdc.lt>
2026-06-03 20:58:55 +0300

    chore: config
    remove preview, set 80 as default

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173,
-    allowedHosts: [
-      "ec2-51-20-182-189.eu-north-1.compute.amazonaws.com",
-    ],
-    proxy: {
-      "/api": {
-        target: "http://51.21.218.154:8080",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-  preview: {
-    host: true,
     port: 80,
     allowedHosts: [
       "ec2-51-20-182-189.eu-north-1.compute.amazonaws.com",
